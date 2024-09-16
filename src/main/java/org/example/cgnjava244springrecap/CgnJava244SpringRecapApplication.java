e8b79fa18f09b4c5275371860af4e88b8fd30360
AhmedMGod <ahmed_mumin.god@outlook.de>
2024-09-16 14:29:13 +0200

    add frontend file to static file

---
+++ src/main/java/org/example/cgnjava244springrecap/CgnJava244SpringRecapApplication.java
@@ -0,0 +1,13 @@
+package org.example.cgnjava244springrecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgnJava244SpringRecapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgnJava244SpringRecapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cgnjava244springrecap/CgnJava244SpringRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cgnjava244springrecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgnJava244SpringRecapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
